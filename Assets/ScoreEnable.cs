9569302c6fbb182925c862363aeb89195d11ab1e
lucasbegnini <lucasbegnini@gmail.com>
2016-02-02 14:06:48 -0200

    [Begnini] tentativa de ajeitar o BestScore

--- Assets/ScoreEnable.cs
+++ Assets/ScoreEnable.cs
@@ -9,7 +9,7 @@ public class ScoreEnable : MonoBehaviour {
         if (gameObject.name == "ScoreText")
         { GetComponent<Text>().text = GameObject.FindGameObjectWithTag("GameScore").GetComponent<GameScore>().getScore().ToString();  }
         if (gameObject.name == "BestScore")
-        { GetComponent<Text>().text = PlayerPrefs.GetInt("BestScore").ToString(); }
+        { GetComponent<Text>().text = (PlayerPrefs.GetInt("BestScore")+1).ToString(); }
 
        // this.gameObject.SetActive(false);
 
